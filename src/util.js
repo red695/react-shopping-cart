5e58e1a43a41a05bd6f69e3d99efb7c82f1f1761
red695 <66657482+red695@users.noreply.github.com>
2020-06-08 23:56:38 -0400

    shopping app

---
+++ src/util.js
@@ -0,0 +1,14 @@
+export const formatPrice = (x, currency) => {
+  switch(currency) {
+    case 'BRL':
+      return x.toFixed(2).replace('.', ',');
+    default:
+      return x.toFixed(2);
+  }
+};
+
+
+export default {
+    formatPrice,
+}
+export const productsAPI = 'products.json';
